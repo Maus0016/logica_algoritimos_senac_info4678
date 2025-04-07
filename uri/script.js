925f0cceb935f9a5acbf7ab72928288fb297e820
Daniel Maus <hakumennomodo@gmail.com>
2025-04-07 19:54:34 -0300

    salvar

--- uri/script.js
+++ uri/script.js
@@ -1,5 +1,8 @@
-var numero = parseFloat(lines[0]);
-var horas = parseFloat(lines[1]);
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+var numero = parseInt(lines[0]);
+var horas = parseInt(lines[1]);
 var salario = parseFloat(lines[2]);
 var total1 = horas * salario;
-console.log( "NUMBER = " + numero ,'\n' +"SALARY = U$"  + total1.toFixed(2));
+console.log("NUMBER = " +numero+ '\n' + "SALARY = U$ " + total1.toFixed(2));
